804abbf68b850948508598456df36a32b6630d01
Vladimir <volodymyr@kyryliuk.im>
2014-11-11 22:39:21 -0800

    Added more files

---
+++ client/ui/RandomTask.jsx
@@ -0,0 +1,33 @@
+/** @jsx React.DOM */
+
+var React = require('React');
+
+var Button = require('./common/Button.jsx');
+var Task = require('./Task.jsx');
+
+var RandomTask = React.createClass({
+  getInitialState: function() {
+    return {};
+  },
+
+  _randomTask: function() {
+    $.get('/task', function(result) {
+      var i = Math.floor(Math.random() * result.length);
+      this.setState({task: result[i]});
+    }.bind(this))
+  },
+
+  render: function() {
+    var task = this.state.task && <Task task={this.state.task} />;
+
+    return (
+      <fieldset>
+        <legend>I am procrastinating...</legend>
+        <Button label="What to do" onClick={this._randomTask} />
+        {task}
+      </fieldset>
+    );
+  }
+});
+
+module.exports = RandomTask;
